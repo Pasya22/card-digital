e361044ef98e958adc5bbdd20101c6d9cb285600
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2023-09-09 21:48:01 +0700

    launch

---
+++ application/models/MSudi_model.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+                        
+class MSudi_model extends CI_Model 
+{
+    public function select()
+    {
+
+    }                        
+                        
+}
+
+
+/* End of file MSudi_model.php and path \application\models\MSudi_model.php */
